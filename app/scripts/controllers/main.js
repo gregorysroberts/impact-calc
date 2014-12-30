b979a2f68ac8566e5c4b0550831be11767c6b821
gregorysroberts <gregorysroberts@gmail.com>
2014-12-30 11:50:12 -0700

    Adds functionality for custom messaging depending on team selected

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -22,19 +22,54 @@ angular.module('calcApp')
       var monthlyDeals = weeklyDeals * 4;
       var avgDeals = $scope.user.dealSize;
       $scope.master.annualRevenue = monthlyDeals * avgDeals * reps * 12;
-      console.log($scope.master.annualRevenue);
       incrementalRevenue ();
+
+      // Depending on their team set the focus and the indicators of success
+      if ($scope.master.team == "an inbound LDR") {
+        $scope.master.focus = "qualifying opportunities for the closing team.";
+        $scope.master.indicators = "response time, engagement rate, call volume,  and email volume";
+      }
+      if ($scope.master.team == "an outbound LDR") {
+        $scope.master.focus = "identifying qualifying opportunities for the closing team.";
+        $scope.master.indicators = "call & email volume, engagement rate, and lead prioritization"
+      }
+      if ($scope.master.team == "a mixed LDR") {
+        $scope.master.focus = "identifying and qualifying opportunities for the closing team.";
+        $scope.master.indicators = "response time, call & email volume, engagement rate, and lead prioritization";
+      }
+      if ($scope.master.team == "an integrated inside sales") {
+        $scope.master.focus = "identifying and qualifying high-potential opportunties and closing them.";
+        $scope.master.indicators = "call & email volume, engagement rate, and lead prioritization";
+      }
+      if ($scope.master.team == "a closers only") {
+        $scope.master.focus = "closing high-quality business with qualified opportunities.";
+        $scope.master.indicators = "clear visibility into engagement and lead prioritization";
+      }
+
     };
 
    
 
     var incrementalRevenue = function () {
+
+      $scope.master.increase = $scope.master.annualRevenue;
+
       if ($scope.master.increaseCalls == "true") {
-         var currentRevenue = $scope.master.annualRevenue;
-    console.log(currentRevenue);
-        $scope.master.increase = currentRevenue * 1.25;
-        console.log($scope.master.increase);
+        $scope.master.increase = $scope.master.increase * 1.25;
       }
+
+      if ($scope.master.increaseConversion == "true") {
+        $scope.master.increase = $scope.master.increase * 1.12;
+      }
+
+      if ($scope.master.increaseQuota == "true") {
+        $scope.master.increase = $scope.master.increase * 1.1;
+      }
+
+      console.log($scope.master);
+
     }
+
+
 
   });
